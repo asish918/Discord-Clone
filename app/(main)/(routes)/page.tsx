1813a2ac36687fc9996c145fabea29837d3bd178
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-08-27 12:49:32 +0530

    Implemented Dark Mode Toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }

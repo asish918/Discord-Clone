fb2d490e623e6fe79157905dd586f1a4d8988d2e
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-08-30 01:19:35 +0530

    Implemented Server Invites

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import CreateServerModal from "@/components/modals/CreateServerModal"
 import { useEffect, useState } from "react"
+import InviteModal from "@/components/modals/InviteModal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +18,7 @@ export const ModalProvider = () => {
     return (
         <>
             <CreateServerModal />
+            <InviteModal />
         </>
     )
 }

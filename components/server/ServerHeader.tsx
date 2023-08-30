77619a4c2317df1948f4ef08be81e0f178b6cb53
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-08-30 14:22:50 +0530

    Implemented Server Settings and Member Mngmt

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -40,14 +40,14 @@ const ServerHeader = ({
                 )}
 
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer" onClick={() => onOpen("editServer", { server })}>
                         Server Settings
                         <Settings className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
 
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer" onClick={() => onOpen("members", { server })}>
                         Manage Members
                         <Users className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>

d9a5e28b558aa6feafec659c8e3d6e91e87fb345
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-09-01 23:29:55 +0530

    Added Input and API for sending Messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/ChatHeader";
+import ChatInput from "@/components/chat/ChatInput";
 import { currentProfile } from "@/lib/currentProfile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -39,6 +40,18 @@ const ChannelIdPage = async ({
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+
+            <div className="flex-1">Future Messages</div>
+
+            <ChatInput 
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId
+                }}
             />
         </div>
     )
